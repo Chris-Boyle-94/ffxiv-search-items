428515b3a48c9c9bab34d048f437fc355d1f669d
Christopher Boyle <cboyle824@gmail.com>
2021-12-31 19:04:09 -0500

    added more styling, need to clean up some other things and figure out what i want to do with the spinner component since it doesnt really look great

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,31 @@
 import { Link } from "react-router-dom";
 import SearchForm from "./SearchForm";
+import { connect } from "react-redux";
+import { click } from "../actions";
+import { useHistory } from "react-router-dom";
 import moogle from "../imgs/moogle.jpg";
 
-const Header = () => {
+const Header = ({ click }) => {
     const token = localStorage.getItem("token");
+    const history = useHistory();
 
     const logout = () => {
         localStorage.removeItem("token");
         localStorage.removeItem("user_id");
     };
 
+    const handleClick = () => {
+        click();
+        history.push("/");
+    };
+
     return (
         <div className="header">
             <div className="header__logo-title">
                 <img className="header__logo" src={moogle} alt="moogle" />
-                <h1 className="header__title">Moghead</h1>
+                <h1 className="header__title" onClick={handleClick}>
+                    Moghead
+                </h1>
             </div>
             <SearchForm />
             <nav className="header__container">
@@ -46,4 +57,4 @@ const Header = () => {
     );
 };
 
-export default Header;
+export default connect(null, { click })(Header);
