129442ce31fee95ac282bae43fa6171ce9b2c071
Christopher Boyle <cboyle824@gmail.com>
2022-01-01 05:31:06 -0500

    started building a skeleton loader in place of the spinner saying 'loading'

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -1,5 +1,13 @@
+const dummyArray = ["", "", "", "", "", ""];
+
 const Spinner = () => {
-    return <h2>Loading...</h2>;
+    return (
+        <div classname="spinner">
+            {dummyArray.map(() => {
+                return <div className="spinner__card"></div>;
+            })}
+        </div>
+    );
 };
 
 export default Spinner;
