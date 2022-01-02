b46494e504ca3f3a320128151313719b43d27e0e
Christopher Boyle <cboyle824@gmail.com>
2022-01-01 19:55:18 -0500

    skeleton loader looks good

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -1,11 +1,7 @@
-const dummyArray = ["", "", "", "", "", ""];
-
 const Spinner = () => {
     return (
         <div classname="spinner">
-            {dummyArray.map(() => {
-                return <div className="spinner__card"></div>;
-            })}
+            <div className="spinner__card"></div>;
         </div>
     );
 };
