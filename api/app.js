cf03599bc68399dcae766ea14e898224de0510d1
Christopher Boyle <cboyle824@gmail.com>
2022-01-11 14:51:18 -0500

    reenable cors

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const helmet = require("helmet");
 
 const itemsRouter = require("./items/items-router");
@@ -9,6 +10,7 @@ const favoritesRouter = require("./favorites/favorites-router");
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 server.use(
     helmet({
         contentSecurityPolicy: false,
