89b13a82abb43a00c727061e063dad0b66898994
Christopher Boyle <cboyle824@gmail.com>
2022-01-10 20:01:30 -0500

    almost

--- api/app.js
+++ api/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const cors = require("cors");
+// const cors = require("cors");
 const helmet = require("helmet");
 
 const itemsRouter = require("./items/items-router");
