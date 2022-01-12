3588aae02e9927d3dcd2e66272210a03c73e8cff
Christopher Boyle <cboyle824@gmail.com>
2022-01-12 16:31:33 -0500

    item cards now are all the same height when viewing at the laptop breakpoint and above. searchbar also gets bigger with the screen width at the mobile level

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
-import logger from "redux-logger";
 import thunk from "redux-thunk";
 import { reducer } from "./reducer";
 import "./Sass/style.scss";
 
-const store = createStore(reducer, applyMiddleware(thunk, logger));
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
     <React.StrictMode>
