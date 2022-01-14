5d3027efcfe29352ff8204cb3e13301683359820
Christopher Boyle <cboyle824@gmail.com>
2022-01-14 18:43:17 -0500

    changed path.join to path.resolve and added 'build' as an argument

--- api/app.js
+++ api/app.js
@@ -23,8 +23,10 @@ server.use("/favorites", favoritesRouter);
 
 if (process.env.NODE_ENV === "production") {
     server.use(express.static(path.resolve(__dirname, "../client/build")));
-    server.get("/*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../client/public", "index.html"));
+    server.get("*", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "../client/public", "build", "index.html")
+        );
     });
 }
 
