d29d8f883ba23b4e503577e622ccdeab79bdf49c
Christopher Boyle <cboyle824@gmail.com>
2022-01-14 00:51:25 -0500

    added a missing click handler on Home header link

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -41,7 +41,11 @@ const Header = ({ click, setLoggedIn, isLoggedIn, search }) => {
             {isLoggedIn || token ? (
                 <nav className="header__container">
                     <li>
-                        <Link className="header__text" to="/">
+                        <Link
+                            className="header__text"
+                            to="/"
+                            onClick={handleClick}
+                        >
                             Home
                         </Link>
                     </li>
